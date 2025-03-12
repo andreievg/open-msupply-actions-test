223490ae6d15c56fb22472ff1044ffeac16e8540
Andrei <andrei.evguenov@gmail.com>
2025-03-12 16:46:28 +1300

    Back to release

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
 
 FROM rust:slim
 
-RUN apt-get update && apt-get install -y faketime
+RUN apt-get update && apt-get install git make
+
+# Install faketime
+WORKDIR /usr/src/
+RUN git clone https://github.com/wolfcw/libfaketime.git
+WORKDIR /usr/src/libfaketime/src
+RUN make install
+# This will make libfaketime.so.1 run when time is requested form the system
+# making FAKETIME env variable work
+RUN echo "/usr/src/libfaketime/src/libfaketime.so.1" > /etc/ld.so.preload
 
 WORKDIR /usr/src/omsupply/server
 # Copy executable
-COPY server/target/debug/remote_server .
-COPY server/target/debug/remote_server_cli .
+COPY server/target/release/remote_server .
+COPY server/target/release/remote_server_cli .
 # Copy entry
 COPY docker/entry.sh .
 # Copy reference file
--- docker/entry.sh
+++ docker/entry.sh
@@ -17,22 +17,10 @@ if [ ! -z "$LOAD_REFERENCE_FILE" ]; then
   MSUPPLY_NO_TEST_DB_TEMPLATE=1 ./remote_server_cli initialise-from-export -n "$LOAD_REFERENCE_FILE"
 fi
 
-
-# Run the remote_server with or without faketime
-if [ ! -z "$FAKE_DATETIME" ]; then
-    # Refresh dates
-    if [ "$SHOULD_REFRESH_DATES" = true ]; then
-        echo "Refreshing dates"
-        faketime "$FAKE_DATETIME" ./remote_server_cli refresh-dates
-    fi
-  echo "Starting remote_server with faketime: $FAKE_DATETIME"
-  exec faketime "$FAKE_DATETIME" ./remote_server
-else
-    # Refresh dates
-    if [ "$SHOULD_REFRESH_DATES" = true ]; then
-        echo "Refreshing dates"
-        ./remote_server_cli refresh-dates
-    fi
-  echo "Starting remote_server normally"
-  exec ./remote_server
+# Refresh dates
+if [ "$SHOULD_REFRESH_DATES" = true ]; then
+    echo "Refreshing dates"
+    ./remote_server_cli refresh-dates
 fi
+
+exec ./remote_server
