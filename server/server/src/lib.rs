3db9dce910fe4645567cbabf257d6820fde5ba5b
Andrei <andrei.evguenov@gmail.com>
2025-05-31 20:54:55 +1200

    Dockerise

--- server/server/src/lib.rs
+++ server/server/src/lib.rs
@@ -61,7 +61,7 @@ pub mod print;
 use serve_frontend_plugins::config_server_frontend_plugins;
 use upload::config_upload;
 // Only import discovery for non android features (otherwise build for android targets would fail due to local-ip-address)
-#[cfg(not(target_os = "android"))]
+#[cfg(any(target_os = "macos", target_os = "windows"))]
 mod discovery;
 
 /// Starts the server
@@ -266,8 +266,8 @@ pub async fn start_server(
     info!("Creating graphql schema..done",);
 
     // START DISCOVERY
-    // Don't do discovery in android
-    #[cfg(not(target_os = "android"))]
+    // Only run discovery on Mac or Windows
+    #[cfg(any(target_os = "macos", target_os = "windows"))]
     {
         let discovery_enabled = match settings.server.discovery {
             DiscoveryMode::Disabled => false,
